d6e69473f070908da0104d4bece15e52788dd346
Ivan Boothe <ivan@rootwork.org>
2021-03-17 13:12:42 -0700

    Creating gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 const gulp = require('gulp')
+const rename = require('gulp-rename')
 const mjml = require('gulp-mjml')
 
+const paths = {
+  mjml: {
+    src: './**/index.mjml',
+    dir: '/dist/',
+    dest: '.'
+  }
+};
+
 gulp.task('default', function () {
-  return gulp.src('./**/index.mjml', { base: '.' })
+  return gulp.src(paths.mjml.src)
     .pipe(mjml())
-    .pipe(gulp.dest('./html/'))
+    .pipe(rename(function(path) {
+      path.dirname = path.dirname + paths.mjml.dir;
+      return path;
+    }))
+    .pipe(gulp.dest(paths.mjml.dest))
 });
