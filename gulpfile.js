0453e40ad93715c87db11741308753706677439c
Ivan Boothe <ivan@rootwork.org>
2021-03-17 13:46:49 -0700

    Creating gulp watch, export, default

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp')
+const { src, dest, watch } = require("gulp");
 const rename = require('gulp-rename')
 const mjml = require('gulp-mjml')
 const mjmlEngine = require('mjml')
@@ -11,8 +11,24 @@ const paths = {
   }
 };
 
-gulp.task('default', function () {
-  return gulp.src(paths.mjml.src)
+// MJML -> HTML: Pretty version
+function html_pretty() {
+  return src(paths.mjml.src)
+    .pipe(mjml(mjmlEngine, {
+      beautify: true
+    }))
+    .on('error', e => console.log(e))
+    .pipe(rename(function(path) {
+      path.dirname = path.dirname + paths.mjml.dir;
+      return path;
+    }))
+    .pipe(dest(paths.mjml.dest))
+}
+exports.html_pretty = html_pretty;
+
+// MJML -> HTML: Production version
+function html_prod() {
+  return src(paths.mjml.src)
     .pipe(mjml(mjmlEngine, {
       beautify: false,
       minify: true,
@@ -23,5 +39,21 @@ gulp.task('default', function () {
       path.dirname = path.dirname + paths.mjml.dir;
       return path;
     }))
-    .pipe(gulp.dest(paths.mjml.dest))
-});
+    .pipe(dest(paths.mjml.dest))
+}
+exports.html_prod = html_prod;
+
+// Export for production
+exports.export = async function () {
+  html_prod();
+}
+
+// Watch
+exports.watch = function () {
+  watch("./**/*.mjml", html_pretty);
+}
+
+// Gulp default
+exports.default = async function() {
+  html_pretty();
+}
