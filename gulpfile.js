e4291934561a4ee912a4c03d6a87896f3fbb7dc8
Ivan Boothe <ivan@rootwork.org>
2021-03-17 13:00:06 -0700

    Replacing build scripts with gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-function defaultTask(cb) {
-  console.log("Gulp is working!");
-  cb();
-}
-exports.default = defaultTask;
+const gulp = require('gulp')
+const mjml = require('gulp-mjml')
+
+gulp.task('default', function () {
+  return gulp.src('./**/index.mjml', { base: '.' })
+    .pipe(mjml())
+    .pipe(gulp.dest('./html/'))
+});
--- mjml-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Uses MJML (node) on any `index.mjml` files in first-level subdirectories to
-# render the resulting `index.html` files in-place.
-
-for file in $(find . -maxdepth 2 -type f -name "index.mjml") ; do
-  dir=$(dirname "$file")
-  mjml -r "$file" --config.beautify false --config.minify true --config.keepComments false -o "$dir"/index.html
-done
