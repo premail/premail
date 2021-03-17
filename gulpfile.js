709952207f34e9b5eec7b0335c707a8dbe3d0b85
Ivan Boothe <ivan@rootwork.org>
2021-03-17 13:21:24 -0700

    Setting default output options

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const rename = require('gulp-rename')
 const mjml = require('gulp-mjml')
+const mjmlEngine = require('mjml')
 
 const paths = {
   mjml: {
@@ -12,7 +13,12 @@ const paths = {
 
 gulp.task('default', function () {
   return gulp.src(paths.mjml.src)
-    .pipe(mjml())
+    .pipe(mjml(mjmlEngine, {
+      beautify: false,
+      minify: true,
+      keepComments: false
+    }))
+    .on('error', e => console.log(e))
     .pipe(rename(function(path) {
       path.dirname = path.dirname + paths.mjml.dir;
       return path;
