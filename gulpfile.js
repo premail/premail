153d62bdfd287c03fa8b956d6b225835710e7200
Ivan Boothe <ivan@rootwork.org>
2021-04-06 17:21:33 -0700

    creating cli-based arguments

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,61 @@ const { src, dest, series, parallel, watch } = require('gulp');
 
 const fs         = require('fs')
 const path       = require('path');
+const minimist   = require('minimist');
 const rename     = require('gulp-rename');
 const mjml       = require('gulp-mjml');
 const mjmlEngine = require('mjml');
 const prettier   = require('gulp-prettier');
 
-// @TODO use or remove
-// const prompt     = require('prompt-sync')({ sigint: true });
-// const name = prompt('What is your name? ');
-// console.log(`Hey there ${name}`);
-
 //
 // Config
 //
 
-// Acquire directory information
-const getDirectories = srcPath =>
-  fs.readdirSync(srcPath)
-    .filter(file => fs.lstatSync(path.join(srcPath, file)).isDirectory())
+// Acquire CLI arguments
+const arg = (argList => {
+
+  let arg = {}, a, opt, thisOpt, curOpt;
+  for (a = 0; a < argList.length; a++) {
+
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, '');
+
+    if (opt === thisOpt) {
+
+      // argument value
+      if (curOpt) arg[curOpt] = opt;
+      curOpt = null;
+
+    }
+    else {
+
+      // argument name
+      curOpt = opt;
+      arg[curOpt] = true;
+
+    }
+
+  }
+
+  return arg;
+
+})(process.argv);
+
+// Set constants from CLI arguments
+const designCurrent = arg.d;
+const emailCurrent =  arg.e;
+// @TODO consider replacing this with prompt based on existing directories
+// (see getDirectories, below).
+//
+// const prompt     = require('prompt-sync')({ sigint: true });
+// const name = prompt('What is your name? ');
+// console.log(`Hey there ${name}`);
 
 // Top-level directory for designs
-const designDir = getDirectories('designs');
+const designDir = 'designs';
 
 // Top-level directory for individual emails
-const emailDir = getDirectories('emails');
+const emailDir = 'emails';
 
 // File extensions to process from MJML to HTML
 const mjmlFileExt = 'mjml';
@@ -37,6 +68,17 @@ const styleDir = 'style';
 
 // Subdirectory of emails in which to export HTML code
 const distDir = 'dist';
+
+// Acquire directory information
+const getDirectories = srcPath =>
+  fs.readdirSync(srcPath)
+    .filter(file => fs.lstatSync(path.join(srcPath, file)).isDirectory())
+
+// Get list of designs by directory name
+const designList = getDirectories(designDir);
+
+// Get list of emails by directory name
+const emailList = getDirectories(emailDir);
 
 // Old paths (@TODO rewrite/replace in code below with new ones above.)
 const paths = {
