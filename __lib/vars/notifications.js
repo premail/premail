ca1cf648934cd08edf44e51a76aad30f0d4ff5d9
Ivan Boothe <ivan@rootwork.org>
2021-04-22 18:24:07 -0700

    Brighting up the info notifications

--- __lib/vars/notifications.js
+++ __lib/vars/notifications.js
@@ -9,7 +9,7 @@ const chalk = require('chalk');
 const msg = {
   error: chalk.bgRed.bold.white,
   warn:  chalk.bgYellow.black,
-  info:  chalk.green,
+  info:  chalk.hex('#37fd38'),
   debug: chalk.keyword('aqua'),
   b:     chalk.bold
 }
