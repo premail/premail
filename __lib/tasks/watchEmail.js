1190c75d63edcc9a03ebf3e8477f78f045279d00
Ivan Boothe <ivan@rootwork.org>
2021-05-14 18:09:15 -0700

    WIP: Getting watch tasks to work

--- __lib/tasks/watchEmail.js
+++ __lib/tasks/watchEmail.js
@@ -9,6 +9,7 @@ const { log } = require('../vars/log.js')
 const { msg } = require('../vars/notifications.js')
 
 const buildStyles = require('../tasks/buildStyles.js')
+const buildTemplates = require('../tasks/buildTemplates.js')
 const buildHTML = require('../tasks/buildHTML.js')
 const buildText = require('../tasks/buildText.js')
 /* eslint-enable no-unused-vars */
@@ -16,33 +17,33 @@ const buildText = require('../tasks/buildText.js')
 //
 // Watch design and configuration files and rebuild as necessary.
 //
-module.exports = function watchEmail () {
+module.exports = function watchEmail (done) {
   // Trigger style rebuild.
   watch(
     [config.current.theme.path + config.current.theme.sassDir + '/**/*.scss'],
-    buildStyles
-  )
-    .on('error', e.sassError)
-    .on('change', function (path, stats) {
-      log(msg.info('Styles rebuilt.'))
-    })
+    {
+      delay: 200, // default
+    }
+  ).on('change', function (path, stats) {
+    buildStyles(done)
+    buildTemplates(done)
+    log(msg.info('Styles rebuilt.'))
+  })
 
   // Trigger template rebuild.
-  watch(
-    config.current.path + '/**/*.' + config.user.files.templateExt,
-    buildHTML
-  )
-    .on('error', e.mjmlError)
-    .on('change', function (path, stats) {
+  watch([config.current.path + '/**/*.' + config.user.files.templateExt]).on(
+    'change',
+    function (path, stats) {
+      buildHTML(done)
       log(msg.info('HTML rebuilt.'))
-    })
+    }
+  )
 
   // Trigger text rebuild.
   if (config.user.text.generate) {
-    watch('./**/*.html', buildText)
-      .on('error', e.textError)
-      .on('change', function (path, stats) {
-        log(msg.info('Plain-text rebuilt.'))
-      })
+    watch(['./**/*.html']).on('change', function (path, stats) {
+      buildText(done)
+      log(msg.info('HTML rebuilt.'))
+    })
   }
 }
