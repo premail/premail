f4345507cb16dbfcada7f76cadc2a766c34a1914
Ivan Boothe <ivan@rootwork.org>
2021-05-17 16:09:12 -0700

    WIP: Getting watch tasks to work

--- __lib/tasks/buildTemplates.js
+++ __lib/tasks/buildTemplates.js
@@ -19,7 +19,7 @@ const { debug } = require('../vars/debug.js')
 // Render Handlebars templates into HTML/MJML.
 //
 
-module.exports = function buildTemplates (done) {
+module.exports = function buildTemplates () {
   // Load templates
   const templates = []
   for (const template of config.current.templates.array) {
@@ -27,51 +27,51 @@ module.exports = function buildTemplates (done) {
   }
 
   // Render templates
-  src(templates)
-    // Process Handlebars data
-    .pipe(
-      hb()
-        .partials()
-        .helpers(helpers)
-        .data(config)
-    )
-    .on('error', e.hbError)
-    .on('end', function () {
-      debug('Templates rendered.')
-    })
-
-    // Process file includes
-    .pipe(
-      fileinclude({
-        prefix: '@@',
-        basepath: path.join(
-          config.current.theme.temp,
-          config.current.theme.sassDir
-        ),
-      })
-    )
-    .on('error', e.includeError)
-    .on('end', function () {
-      debug('Template file includes processed.')
-    })
-
-    // Set destination and write files
-    .pipe(
-      rename(function (path, file) {
-        const subPath = file.path
-          .replace(config.current.path, '')
-          .replace(path.basename, '')
-          .replace(path.extname, '')
-        path.dirname += subPath
-        debug('Processed template file: ' + path.basename + path.extname)
-      })
-    )
-    .pipe(dest(config.current.temp))
-    .on('end', function () {
-      debug(
-        msg.b('Created temporary template files at: ') + config.current.temp
+  return (
+    src(templates)
+      // Process Handlebars data
+      .pipe(
+        hb()
+          .partials()
+          .helpers(helpers)
+          .data(config)
       )
-    })
+      .on('error', e.hbError)
+      .on('end', function () {
+        debug('Templates rendered.')
+      })
 
-  done()
+      // Process file includes
+      .pipe(
+        fileinclude({
+          prefix: '@@',
+          basepath: path.join(
+            config.current.theme.temp,
+            config.current.theme.sassDir
+          ),
+        })
+      )
+      .on('error', e.includeError)
+      .on('end', function () {
+        debug('Template file includes processed.')
+      })
+
+      // Set destination and write files
+      .pipe(
+        rename(function (path, file) {
+          const subPath = file.path
+            .replace(config.current.path, '')
+            .replace(path.basename, '')
+            .replace(path.extname, '')
+          path.dirname += subPath
+          debug('Processed template file: ' + path.basename + path.extname)
+        })
+      )
+      .pipe(dest(config.current.temp))
+      .on('end', function () {
+        debug(
+          msg.b('Created temporary template files at: ') + config.current.temp
+        )
+      })
+  )
 }
