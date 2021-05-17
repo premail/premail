f71cb590a01f72fdd2e921c37ee1ed027032c4d5
Ivan Boothe <ivan@rootwork.org>
2021-05-16 17:53:38 -0700

    WIP: Getting watch tasks to work

--- __lib/tasks/watchEmail.js
+++ __lib/tasks/watchEmail.js
@@ -1,7 +1,8 @@
 'use strict'
 
 /* eslint-disable no-unused-vars */
-const { watch, series } = require('gulp')
+const { watch, series, parallel } = require('gulp')
+// https://www.npmjs.com/package/gulp-changed
 
 const { config } = require('../vars/config.js')
 const e = require('../functions/e.js')
@@ -19,42 +20,45 @@ const buildText = require('../tasks/buildText.js')
 //
 
 // Trigger style rebuild.
-function watchStyles (done) {
-  watch(
+function watchStyles () {
+  return watch(
     [config.current.theme.path + config.current.theme.sassDir + '/**/*.scss'],
-    {
-      delay: 200, // default
-    }
-  ).on('change', function (path, stats) {
-    buildStyles(done)
-    buildTemplates(done)
-    log(msg.info('Styles rebuilt.'))
-  })
-}
-
-// Trigger HTML rebuild.
-function watchHTML (done) {
-  watch([config.current.path + '/**/*.' + config.user.files.templateExt]).on(
-    'change',
-    function (path, stats) {
-      buildHTML(done)
-      log(msg.info('HTML rebuilt.'))
-    }
+    buildStyles
+    // {
+    //   delay: 200, // default
+    // },
+    // function () {
+    //   buildStyles(done)
+    //   buildTemplates(done)
+    //   log(msg.info('Styles rebuilt.'))
+    // }
   )
 }
 
-// Trigger text rebuild.
-function watchText (done) {
-  if (config.user.text.generate) {
-    watch(['./**/*.html']).on('change', function (path, stats) {
-      buildText(done)
-      log(msg.info('HTML rebuilt.'))
-    })
-  }
-}
+// Trigger HTML rebuild.
+// function watchHTML (done) {
+// watch([config.current.path + '/**/*.' + config.user.files.templateExt]).on(
+//   'change',
+//   function (path, stats) {
+//     buildHTML(done)
+//     log(msg.info('HTML rebuilt.'))
+//   }
+// )
+// }
 
-module.exports = {
-  watchStyles,
-  watchHTML,
-  watchText,
+// Trigger text rebuild.
+// function watchText (done) {
+// if (config.user.text.generate) {
+//   watch(['./**/*.html']).on('change', function (path, stats) {
+//     buildText(done)
+//     log(msg.info('HTML rebuilt.'))
+//   })
+// }
+// }
+
+module.exports = function watchEmail () {
+  series(
+    log(msg.info('\n⌚ ' + msg.b('WATCHING') + ' ⌚\n')),
+    parallel(watchStyles)
+  )
 }
