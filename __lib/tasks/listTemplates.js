ab5c2db997d877b94b58d39c5817b86ad6992bad
Ivan Boothe <ivan@rootwork.org>
2021-05-18 15:13:07 -0700

    Fix listTemplates

--- __lib/tasks/listTemplates.js
+++ __lib/tasks/listTemplates.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /* eslint-disable no-unused-vars */
-const config = require('../vars/config.js')
+const { config } = require('../vars/config.js')
 const { log, msg, debug } = require('../vars/notify.js')
 /* eslint-enable no-unused-vars */
 
@@ -10,6 +10,10 @@ const { log, msg, debug } = require('../vars/notify.js')
 //
 
 module.exports = async function listTemplates () {
-  log(msg.debug(msg.b('Main template file:\n') + config.templates.main + '\n'))
-  log(msg.debug(msg.b('Partials:\n') + config.templates.partials))
+  log(
+    msg.debug(
+      msg.b('Main template file:\n') + config.current.templates.main + '\n'
+    )
+  )
+  log(msg.debug(msg.b('Partials:\n') + config.current.templates.partials))
 }
