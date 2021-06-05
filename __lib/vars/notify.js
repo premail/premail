7d2431f4f801be58a8cd48e8b29757097d452c4a
Ivan Boothe <ivan@rootwork.org>
2021-06-04 19:20:06 -0700

    Improving json/unjson console message displays

--- __lib/vars/notify.js
+++ __lib/vars/notify.js
@@ -34,13 +34,13 @@ function msg (type, message, title = null) {
     case 'error':
       symbolFormat = '\n ' + symbols.cross
       if (title) {
-        titleFormat = console.log(
+        titleFormat = console.error(
           ' ' + colors.bgRed.white.bold(symbolFormat + ' ' + title + ' ') + ' '
         )
-        messageFormat = console.log(colors.red('   ' + message))
+        messageFormat = console.error(colors.red('   ' + message))
       } else {
         titleFormat = null
-        messageFormat = console.log(
+        messageFormat = console.error(
           ' ' + colors.red.bold(symbolFormat + ' ' + message) + ' \n'
         )
       }
@@ -48,15 +48,15 @@ function msg (type, message, title = null) {
     case 'warn':
       symbolFormat = '\n ' + symbols.warning
       if (title) {
-        titleFormat = console.log(
+        titleFormat = console.warn(
           ' ' +
             colors.bgYellow.black.bold(symbolFormat + ' ' + title + ' ') +
             ' '
         )
-        messageFormat = console.log(colors.yellow('   ' + message))
+        messageFormat = console.warn(colors.yellow('   ' + message))
       } else {
         titleFormat = null
-        messageFormat = console.log(
+        messageFormat = console.warn(
           ' ' + colors.bgYellow.black(symbolFormat + ' ' + message) + ' \n'
         )
       }
@@ -105,17 +105,17 @@ function msg (type, message, title = null) {
       }
       break
     default:
-      console.log(
+      console.error(
         '\n\x1b[1;41;37mERROR: Notification type not recognized.\x1b[0m'
       )
       if (typeof type === 'object' && type.constructor === Object) {
-        console.log(
+        console.error(
           '\x1b[31mSupplied value-by-reference was: "' +
             Object.keys(type) +
             '"\x1b[0m\n'
         )
       } else {
-        console.log('\x1b[31mSupplied value was: "' + type + '"\x1b[0m\n')
+        console.error('\x1b[31mSupplied value was: "' + type + '"\x1b[0m\n')
       }
   }
 
@@ -135,8 +135,9 @@ function json (object, title = null) {
   if (title) {
     objectFormatted += symbols.info + ' ' + colors.bold(title) + ' \n'
   }
-  objectFormatted += ' ' + JSON.stringify(object, null, 2) + ' \n'
-  return console.log(colors.green(objectFormatted))
+  objectFormatted +=
+    '\n ' + console.dir(object, { depth: null, colors: true }) + ' \n'
+  return objectFormatted
 }
 
 function unjson (object, title = null) {
@@ -144,8 +145,16 @@ function unjson (object, title = null) {
   if (title) {
     objectFormatted += symbols.info + ' ' + colors.bold(title) + ' \n'
   }
+  const objectFlattened = JSON.stringify(object, null, 2)
+
   objectFormatted +=
-    ' ' + JSON.stringify(object, null, 2).replace(/["{},]/g, '') + ' \n'
+    ' ' +
+    objectFlattened
+      .replace(/["{},]/g, '')
+      .replace(/[[]/g, '')
+      .replace(/[\]]/g, '')
+      .replace(/^ {2}/gm, '') +
+    ' \n'
   return console.log(colors.cyan(objectFormatted))
 }
 
