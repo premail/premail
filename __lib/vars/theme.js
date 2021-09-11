e304c1a02e5aa15b5ffea74b2bd79a5ad2c8e443
Ivan Boothe <ivan@rootwork.org>
2021-09-10 18:15:21 -0700

    Adding a lot of theming options
    Signed-off-by: Ivan Boothe <ivan@rootwork.org>

--- __lib/vars/theme.js
+++ __lib/vars/theme.js
@@ -12,14 +12,18 @@ const theme = config.theme
 
 // Because we can't access nested objects in the Sass files, we need to define
 // and export each object in turn.
+const page = theme.page
 const colors = theme.colors
 const fonts = theme.fonts
-const text = theme.text
+const text = theme.text.default
+const links = theme.links
 const typography = config.user.details.typography
 
 module.exports = {
+  page,
   colors,
   fonts,
   text,
+  links,
   typography,
 }
