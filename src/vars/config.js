1b1de5a1993dc9b940f5811bfdf025b54a039f52
Ivan Boothe <ivan@rootwork.org>
2021-10-27 15:35:16 -0700

    Update config, theme, templates to match new directory structure
    Signed-off-by: Ivan Boothe <ivan@rootwork.org>

--- src/vars/config.js
+++ src/vars/config.js
@@ -40,14 +40,6 @@ const userJSON = yaml.loadAll(
 )
 config.user = userJSON[0]
 
-// Save template filename parts.
-const templateDotExt = path.extname(config.user.files.template)
-config.user.files.templateExt = templateDotExt.replace('.', '')
-const templateFilename = path.basename(
-  config.user.files.template,
-  path.extname(config.user.files.template)
-)
-
 // Set current folders based on CLI flags, if any are set.
 let currentDesign = config.user.folders.design.default
 
@@ -95,6 +87,7 @@ if (currentEmail) {
 }
 
 // Set current theme path
+// @TODO This likely needs to be moved once different themes can be defined.
 config.current.theme = {
   name: config.user.folders.theme.dir,
   path: projectPath(
@@ -104,111 +97,6 @@ config.current.theme = {
     config.user.folders.theme.dir
   ),
 }
-
-// Load theme config from YAML file.
-const themeYAML = fs.readFileSync(
-  path.join(config.current.theme.path, config.file.theme),
-  {
-    encoding: 'utf-8',
-  }
-)
-const themeJSON = yaml.loadAll(themeYAML)
-config.theme = themeJSON[0]
-
-// Calculating internal-only (not included in config file) theme settings.
-if (config.theme.fonts) {
-  // Font stack
-  config.theme.fonts.stack.first = ''
-
-  // Web font
-  config.theme.fonts.web = false
-  if (
-    config.theme.fonts.stack.google.enabled ||
-    config.theme.fonts.stack.custom.enabled
-  ) {
-    config.theme.fonts.web = true
-  }
-
-  // Custom Font URI
-  if (config.theme.fonts.stack.custom.enabled) {
-    config.theme.fonts.stack.first = config.theme.fonts.stack.custom.name
-  }
-
-  // Google Font URI
-  if (config.theme.fonts.stack.google.enabled) {
-    config.theme.fonts.stack.first = config.theme.fonts.stack.google.name
-    const weights = []
-    let specs
-
-    if (config.theme.fonts.stack.google.italics) {
-      specs = 'ital,wght@'
-
-      for (let weight of config.theme.fonts.stack.google.weights) {
-        weight = '0,' + weight
-        weights.push(weight)
-      }
-
-      for (let weight of config.theme.fonts.stack.google.weights) {
-        weight = '1,' + weight
-        weights.push(weight)
-      }
-    } else {
-      specs = 'wght@'
-
-      for (const weight of config.theme.fonts.stack.google.weights) {
-        weights.push(weight)
-      }
-    }
-
-    specs += weights.reduce(
-      (s, x, i) => s + (i > 0 ? ';' : '') + (x == null ? '' : x),
-      ''
-    )
-
-    config.theme.fonts.stack.google.href =
-      "'https://fonts.googleapis.com/css2?family=" +
-      config.theme.fonts.stack.google.name.replace(/\s/g, '+') +
-      ':' +
-      specs +
-      "&amp;display=swap'"
-  }
-}
-
-if (config.theme.head) {
-  config.theme.head.array = ''
-  for (const i of config.theme.head) {
-    config.theme.head.array += i.html
-  }
-}
-
-// Set current template paths
-config.current.templates = {}
-
-config.current.templates.array = getFiles(
-  config.current.path + path.sep,
-  config.user.files.templateExt
-)
-config.current.templates.list = config.current.templates.array
-  .toString()
-  .split(',')
-  .join('\n')
-config.current.templates.main = path.join(
-  config.current.path,
-  config.current.mainTemplate
-)
-config.current.templates.all = [
-  config.current.templates.main,
-  ...config.current.templates.array,
-]
-
-// Define intermediate rendered template -- post-Handlebars, pre-MJML
-config.current.templates.int = path.join(
-  config.current.dist,
-  `${templateFilename}.mjml`
-)
-
-// Create a container for rendered partials
-config.partials = {}
 
 module.exports = {
   config,
--- src/vars/theme.js
+++ src/vars/theme.js
@@ -1,13 +1,94 @@
 'use strict'
 
 /* eslint-disable no-unused-vars */
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+
 const { config } = require('../vars/config.js')
 /* eslint-enable no-unused-vars */
 
 //
-// Create a theme-only export for use in importing values to Sass
+// Set up theme configuration
 //
 
+// Load theme config from YAML file.
+const themeYAML = fs.readFileSync(
+  path.join(config.current.theme.path, config.file.theme),
+  {
+    encoding: 'utf-8',
+  }
+)
+const themeJSON = yaml.loadAll(themeYAML)
+config.theme = themeJSON[0]
+
+// Calculating internal-only (not included in config file) theme settings.
+if (config.theme.fonts) {
+  // Font stack
+  config.theme.fonts.stack.first = ''
+
+  // Web font
+  config.theme.fonts.web = false
+  if (
+    config.theme.fonts.stack.google.enabled ||
+    config.theme.fonts.stack.custom.enabled
+  ) {
+    config.theme.fonts.web = true
+  }
+
+  // Custom Font URI
+  if (config.theme.fonts.stack.custom.enabled) {
+    config.theme.fonts.stack.first = config.theme.fonts.stack.custom.name
+  }
+
+  // Google Font URI
+  if (config.theme.fonts.stack.google.enabled) {
+    config.theme.fonts.stack.first = config.theme.fonts.stack.google.name
+    const weights = []
+    let specs
+
+    if (config.theme.fonts.stack.google.italics) {
+      specs = 'ital,wght@'
+
+      for (let weight of config.theme.fonts.stack.google.weights) {
+        weight = '0,' + weight
+        weights.push(weight)
+      }
+
+      for (let weight of config.theme.fonts.stack.google.weights) {
+        weight = '1,' + weight
+        weights.push(weight)
+      }
+    } else {
+      specs = 'wght@'
+
+      for (const weight of config.theme.fonts.stack.google.weights) {
+        weights.push(weight)
+      }
+    }
+
+    specs += weights.reduce(
+      (s, x, i) => s + (i > 0 ? ';' : '') + (x == null ? '' : x),
+      ''
+    )
+
+    config.theme.fonts.stack.google.href =
+      "'https://fonts.googleapis.com/css2?family=" +
+      config.theme.fonts.stack.google.name.replace(/\s/g, '+') +
+      ':' +
+      specs +
+      "&amp;display=swap'"
+  }
+}
+
+if (config.theme.head) {
+  config.theme.head.array = ''
+  for (const i of config.theme.head) {
+    config.theme.head.array += i.html
+  }
+}
+
+// Create a theme-only export for use in importing values to Sass
 const theme = config.theme
 
 // Because we can't access nested objects in the Sass files, we need to define
