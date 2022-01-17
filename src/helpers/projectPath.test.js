7986a4ae6724453de03ba0b3355a858e15f0594e
Ivan Boothe <git@rootwork.org>
2022-01-16 19:33:37 -0800

    removing non-windows compatible tests
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- src/helpers/projectPath.test.js
+++ src/helpers/projectPath.test.js
@@ -8,11 +8,5 @@ describe('projectPath()', () => {
     it(`returns a value for "${item.cwd}, ${item.path}"`, () => {
       expect(projectPath(arg)).toBeTruthy()
     })
-    it(`returns an absolute directory for "${item.cwd}, ${item.path}"`, () => {
-      expect(projectPath(arg)).toMatch(/^\//)
-    })
-    it(`returns "${item.result}" for "${item.cwd}, ${item.path}"`, () => {
-      expect(projectPath(arg)).toBe(item.result)
-    })
   })
 })
